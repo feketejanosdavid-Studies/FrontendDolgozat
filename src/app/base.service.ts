d716d165d13ed7a5841ded153db040bda7c0bd60
Fekete János Dávid <janosdavidfekete@gmail.com>
2025-01-13 22:43:04 +0100

    base.service-ben a CRUD kész

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
+  url = 'https://dolgozat-79584-default-rtdb.europe-west1.firebasedatabase.app'
+  private dataSubject = new Subject();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.loadAllDatas();
+   }
+
+   loadAllDatas() {
+    return this.http.get(this.url).subscribe(
+      (data) => this.dataSubject.next(data)
+    )
+   }
+
+   getAllDatas() {
+    return this.dataSubject
+   }
+
+   createNewData(data: any) {
+    return this.http.post(this.url, data).subscribe(
+      () => this.loadAllDatas())
+   }
+
+   updateData(id: number, data: any) {
+    return this.http.put(this.url+id, data).subscribe(
+      () => this.loadAllDatas())
+   }
+
+   deleteData(id: number) {
+    return this.http.delete(this.url+id).subscribe(
+      () => this.loadAllDatas())
+   }
 }
